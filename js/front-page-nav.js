653306501d13dda051fb598dc599f2337fdd5bcc
Megumi Takashima <mtakashima00@mylangara.ca>
2021-08-05 11:34:09 -0700

    hide console.log

--- js/front-page-nav.js
+++ js/front-page-nav.js
@@ -1,6 +1,6 @@
-console.log("connected front-page-nav.js");
+// console.log("connected front-page-nav.js");
 
-
+//Device Judge
 function isMobile() {
     if (navigator.userAgent.match(/iPhone|iPad|Android.+Mobile/)) {
       return true;
@@ -9,12 +9,13 @@ function isMobile() {
     }
 }
 
-
+//Define menus in front-page
 const menus = document.querySelectorAll("body.page-front main .wp-block-group:nth-child(2) .wp-block-group__inner-container .wp-block-cover .wp-block-cover__inner-container p a");
 
-console.log(menus);
+// console.log(menus);
 
 
+//if mobile, show the menu text when the page loads
 if(isMobile()){
     window.addEventListener("load", ()=> {
         for(i=0; i<menus.length; i++){
--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-console.log("connected navigation.js");
+// console.log("connected navigation.js");
 
 
 // Click event for main menu in mobile view
